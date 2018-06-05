b0fdf9a5d6bb0a193e12acf97c738373ce52d732
luizguilhermefr <luizguilhermefr@gmail.com>
2018-06-05 14:33:58 -0300

    this is how we generate our shit.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
 import random
+
 
 class Senoidal:
     LOWER_INTERVAL = 0.0
@@ -8,8 +10,11 @@ class Senoidal:
     def __init__(self):
         self.population = []
 
+    def get_value(self, x):
+        return x * math.sin(10 * math.pi * x) + 5
+
     def generate_initial_population(self, size):
-        for i in range(0, int(size)):
+        for i in range(0, size):
             rand_num = random.uniform(self.LOWER_INTERVAL, self.UPPER_INTERVAL)
             self.population.append(rand_num)
 
@@ -17,11 +22,44 @@ class Senoidal:
         for item in self.population:
             print(str(item))
 
-alpha = input('Alpha: ')
-repetitions = input('Repetitions: ')
-pop_size = input('Population size: ')
-lower_bound = input('Lower bound: ')
-upper_bound = input('Upper bound: ')
+
+class Solver:
+    def __init__(self, problem, lower_bound, upper_bound):
+        self.problem = problem
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+
+    def perturbate_current(self):
+        pass
+
+    def run(self):
+        self.perturbate_current()
+
+
+try:
+    alpha = float(raw_input('Alpha [0.9]: '))
+except ValueError:
+    alpha = 0.9
+
+try:
+    repetitions = int(raw_input('Repetitions [10]: '))
+except ValueError:
+    repetitions = 10
+
+try:
+    pop_size = int(raw_input('Population size [10]: '))
+except ValueError:
+    pop_size = 10
+
+try:
+    lower_bound = float(raw_input('Lower bound [27]: '))
+except ValueError:
+    lower_bound = 27
+
+try:
+    upper_bound = float(raw_input('Upper bound [1538]:'))
+except ValueError:
+    upper_bound = 1538
 
 senoidal_problem = Senoidal()
 senoidal_problem.generate_initial_population(pop_size)
